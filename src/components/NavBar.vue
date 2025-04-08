93d25bcb0a1e0ec33eababc1d17e89350deaf9d0
Mark Davich <Mark.Davich@its.idaho.gov>
2025-04-08 15:47:17 -0600

    Added some instructions for the console window

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="nav-bar">
-    <div class="title">Alphabet Soup</div>
+    <div class="title">Alphabet Soup
+    </div>
+    <div class="shortcut">
+      Press <span class="key">Ctrl</span> + <span class="key">Shift</span> + <span class="key">I</span> to check out the
+      data in the <u><i>console</i></u> tab.
+    </div>
     <router-link class="link" v-if="route === 'About'" to="/">WordSearch</router-link>
     <router-link class="link" v-if="route === 'WordSearch'" to="/about">About</router-link>
+
   </div>
 </template>
 
@@ -35,5 +41,19 @@ export default {
 
 .link {
   color: var(--white);
+}
+
+.shortcut {
+  font-size: 0.9em;
+  color: var(--white);
+}
+
+.key {
+  background-color: var(--vt-c-text-light-2);
+  color: var(--white);
+  padding: 2px 5px;
+  border-radius: 3px;
+  font-family: monospace;
+  margin: 0 2px;
 }
 </style>
